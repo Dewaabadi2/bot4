c3dcdd7f004fdf6bd523dbc47f67bdcb37d234d0
Dewaabadi2 <bsmjaya30@gmail.com>
2026-08-28 14:09:09 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -109,4 +109,5 @@ if __name__ == "__main__":
         c = i
         #print(f"Proses: {nama} ({email})")
         asyncio.run(main(nama, email, c))
-    print(f"Angka terakhir : {c}")
+    last = contacts[c]['nama']
+    print(f"Angka terakhir : {c} {last}")
